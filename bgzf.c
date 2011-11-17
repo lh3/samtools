b99764528ed99b5e98579b31ddd6adc2d29f07ac
peterjc <p.j.a.cock@googlemail.com>
2011-11-17 17:22:25 +0000

    Fix compression level, not zero for a 28 byte EOF block

--- bgzf.c
+++ bgzf.c
@@ -627,8 +627,8 @@ int bgzf_close(BGZF* fp)
     if (fp->open_mode == 'w') {
         if (bgzf_flush(fp) != 0) return -1;
 		{ // add an empty block
-			// add 28 byes EOF empty block
-			fp->compress_level=0; //don't need original value again
+			// add 28 bytes EOF empty block
+			fp->compress_level=-1; //don't need original value again
 			// (different compression levels would give different empty blocks)
 			int count, block_length = deflate_block(fp, 0);
 #ifdef _USE_KNETFILE
